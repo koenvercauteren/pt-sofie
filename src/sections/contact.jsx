bd7e00082502f15a7d49a871b82f799b4f778b62
Koen Vercauteren <knvercauteren@gmail.com>
2018-09-16 14:45:55 +0200

    Netlify form setup

--- src/sections/contact.jsx
+++ src/sections/contact.jsx
@@ -8,39 +8,104 @@ import ScrollableAnchor from 'react-scrollable-anchor';
 import styles from '../styles/sections/contact.styles';
 import Button from '../components/button';
 
-const Contact = props => {
-  const { classes } = props;
+function encode(data) {
+  return Object.keys(data)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+    .join('&');
+}
 
-  return (
-    <Grid container justify="center" className={classes.container}>
-      <Grid item xs={11} md={10} className={classes.contact}>
-        <Card className={classes.card}>
-          <CardContent className={classes.content}>
-            <ScrollableAnchor id="contact">
-              <Typography variant="display2" className={classes.title}>
-                Contact
-              </Typography>
-            </ScrollableAnchor>
-            <form className={classes.form} name="contact" method="POST" data-netlify="true" s>
-              <TextField required id="name" label="Naam" className={classes.textField} margin="normal" />
-              <TextField required id="email" label="E-mail" className={classes.textField} margin="normal" />
-              <TextField
-                required
-                multiline
-                rowsMax="4"
-                id="email"
-                label="Bericht"
-                className={classes.textField}
-                margin="normal"
-              />
-              <Button type="submit" className={classes.button} text="VERZEND" />
-            </form>
-          </CardContent>
-        </Card>
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      submitState: null,
+      name: null,
+      email: null,
+      message: null,
+    };
+  }
+
+  handleSubmit = e => {
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', ...this.state }),
+    })
+      .then(() => {
+        this.setState({ submitState: 'success' });
+      })
+      .catch(error => {
+        console.error('Failed to submit', error);
+        this.setState({ submitState: 'failed' });
+      });
+    e.preventDefault();
+  };
+
+  handleChange = e => {
+    console.log({ e });
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <Grid container justify="center" className={classes.container}>
+        <Grid item xs={11} md={10} className={classes.contact}>
+          <Card className={classes.card}>
+            <CardContent className={classes.content}>
+              <ScrollableAnchor id="contact">
+                <Typography variant="display2" className={classes.title}>
+                  Contact
+                </Typography>
+              </ScrollableAnchor>
+              <form
+                className={classes.form}
+                name="contact"
+                method="POST"
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+                onSubmit={this.handleSubmit}
+              >
+                <input hidden name="bot-field" onChange={this.handleChange} />
+                <TextField
+                  required
+                  id="name"
+                  name="name"
+                  label="Naam"
+                  className={classes.textField}
+                  margin="normal"
+                  onChange={this.handleChange}
+                />
+                <TextField
+                  required
+                  id="email"
+                  name="email"
+                  label="E-mail"
+                  className={classes.textField}
+                  margin="normal"
+                  onChange={this.handleChange}
+                />
+                <TextField
+                  required
+                  multiline
+                  rowsMax="4"
+                  id="message"
+                  name="message"
+                  label="Bericht"
+                  className={classes.textField}
+                  margin="normal"
+                  onChange={this.handleChange}
+                />
+                <Button type="submit" className={classes.button} text="VERZEND" />
+              </form>
+            </CardContent>
+          </Card>
+        </Grid>
       </Grid>
-    </Grid>
-  );
-};
+    );
+  }
+}
 
 Contact.propTypes = {
   classes: PropTypes.object.isRequired,
