688c48b6ad99c13f1c51064ac7f1697d2c786312
Koen Vercauteren <knvercauteren@gmail.com>
2018-10-11 20:27:27 +0200

    Add instagram logo

--- src/components/header-menu.jsx
+++ src/components/header-menu.jsx
@@ -10,10 +10,11 @@ import { goToTop, goToAnchor } from 'react-scrollable-anchor';
 
 import ANCHORS from '../globals/anchors';
 import Logo from '../assets/img/logo.png';
+import InstagramLogo from '../assets/img/instagram-logo.png';
 
 import Burger from './burger';
 
-const styles = {
+const styles = theme => ({
   root: {
     flexGrow: 1,
   },
@@ -34,59 +35,98 @@ const styles = {
   logo: {
     width: '75px',
   },
-};
+  instagram: {
+    cursor: 'pointer',
+    [theme.breakpoints.down('sm')]: {
+      marginRight: '32px',
+    },
+    [theme.breakpoints.up('xs')]: {
+      marginLeft: '32px',
+    },
+  },
+  instagramWrapper: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
-const HeaderMenu = props => {
-  const { classes } = props;
+class HeaderMenu extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className={classes.root}>
-      <AppBar id="header-menu" className={classes.header} position="fixed" color="primary">
-        <Grid container justify="center">
-          <Grid item xs={11} md={10} className={classes.grid}>
-            <Toolbar disableGutters>
-              <Button color="inherit" onClick={() => goToTop()}>
-                <img className={classes.logo} src={Logo} alt="logo" />
-              </Button>
+    this.state = {
+      window: undefined,
+    };
+  }
 
-              <div className={classes.buttonGroup}>
-                <Hidden xsDown>
-                  <Button color="inherit" onClick={() => goToAnchor(ANCHORS.KEYSTOSUCCESS)}>
-                    Sleutels
-                  </Button>
-                  <Button color="inherit" onClick={() => goToAnchor(ANCHORS.APPROACH)}>
-                    Aanpak
-                  </Button>
-                  <Button color="inherit" onClick={() => goToAnchor(ANCHORS.OFFER)}>
-                    Aanbod
-                  </Button>
-                  <Button color="inherit" onClick={() => goToAnchor(ANCHORS.LOCATION)}>
-                    Locatie
-                  </Button>
-                  <Button color="inherit" onClick={() => goToAnchor(ANCHORS.CONTACT)}>
-                    Contact
-                  </Button>
-                </Hidden>
+  componentDidMount() {
+    this.setState({ window });
+  }
 
-                <Hidden smUp>
-                  <Burger
-                    items={[
-                      { name: 'Sleutels', anchor: ANCHORS.KEYSTOSUCCESS },
-                      { name: 'Aanpak', anchor: ANCHORS.APPROACH },
-                      { name: 'Aanbod', anchor: ANCHORS.OFFER },
-                      { name: 'Locatie', anchor: ANCHORS.LOCATION },
-                      { name: 'Contact', anchor: ANCHORS.CONTACT },
-                    ]}
-                  />
-                </Hidden>
-              </div>
-            </Toolbar>
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <AppBar id="header-menu" className={classes.header} position="fixed" color="primary">
+          <Grid container justify="center">
+            <Grid item xs={11} md={10} className={classes.grid}>
+              <Toolbar disableGutters>
+                <Button color="inherit" onClick={() => goToTop()}>
+                  <img className={classes.logo} src={Logo} alt="logo" />
+                </Button>
+
+                <div className={classes.buttonGroup}>
+                  <Hidden xsDown>
+                    <Button color="inherit" onClick={() => goToAnchor(ANCHORS.KEYSTOSUCCESS)}>
+                      Sleutels
+                    </Button>
+                    <Button color="inherit" onClick={() => goToAnchor(ANCHORS.APPROACH)}>
+                      Aanpak
+                    </Button>
+                    <Button color="inherit" onClick={() => goToAnchor(ANCHORS.OFFER)}>
+                      Aanbod
+                    </Button>
+                    <Button color="inherit" onClick={() => goToAnchor(ANCHORS.LOCATION)}>
+                      Locatie
+                    </Button>
+                    <Button color="inherit" onClick={() => goToAnchor(ANCHORS.CONTACT)}>
+                      Contact
+                    </Button>
+                  </Hidden>
+
+                  <div className={classes.instagramWrapper}>
+                    <img // eslint-disable-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
+                      src={InstagramLogo}
+                      alt="instagram-link"
+                      className={classes.instagram}
+                      onClick={() => {
+                        this.state.window.open('https://www.instagram.com/debacker.sofie/', '_blank');
+                      }}
+                    />
+                  </div>
+
+                  <Hidden smUp>
+                    <Burger
+                      items={[
+                        { name: 'Sleutels', anchor: ANCHORS.KEYSTOSUCCESS },
+                        { name: 'Aanpak', anchor: ANCHORS.APPROACH },
+                        { name: 'Aanbod', anchor: ANCHORS.OFFER },
+                        { name: 'Locatie', anchor: ANCHORS.LOCATION },
+                        { name: 'Contact', anchor: ANCHORS.CONTACT },
+                      ]}
+                    />
+                  </Hidden>
+                </div>
+              </Toolbar>
+            </Grid>
           </Grid>
-        </Grid>
-      </AppBar>
-    </div>
-  );
-};
+        </AppBar>
+      </div>
+    );
+  }
+}
 
 HeaderMenu.propTypes = {
   classes: PropTypes.object.isRequired,
