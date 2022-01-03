dfa5f43b8c80591df5c1d13937563e65e9b1e243
koenvercauteren <knvercauteren@gmail.com>
2022-01-03 15:12:14 +0100

    Update location

--- src/sections/location.jsx
+++ src/sections/location.jsx
@@ -10,27 +10,31 @@ import { FormattedMessage, injectIntl } from 'react-intl';
 import styles from '../styles/sections/location.styles';
 
 const Location = ({ classes }) => (
-    <Grid container justify="center" className={classes.container}>
-        <Grid item xs={11} md={10} className={classes.location}>
-            <ScrollableAnchor id="location">
-                <Typography variant="display2" className={classes.title}>
-                    <FormattedMessage id="location_title" />
-                </Typography>
-            </ScrollableAnchor>
+  <Grid container justify="center" className={classes.container}>
+    <Grid item xs={11} md={10} className={classes.location}>
+      <ScrollableAnchor id="location">
+        <Typography variant="display2" className={classes.title}>
+          <FormattedMessage id="location_title" />
+        </Typography>
+      </ScrollableAnchor>
       <div className={classes.bodyContainer}>
-      <Typography gutterBottom variant="subheading" className={classes.body}>
-                <b><FormattedMessage id="location_content_one" /></b>
-                <FormattedMessage id="location_content_two" />
-                <FormattedMessage id="location_content_three" />
-            </Typography>
-            <Typography gutterBottom variant="subheading" className={classes.body}>
-                <b><FormattedMessage id="location_content_four" /></b>
-                <FormattedMessage id="location_content_five" />
-                <FormattedMessage id="location_content_six" />
-            </Typography>
+        <Typography gutterBottom variant="subheading" className={classes.body}>
+          <b><FormattedMessage id="location_content_one" /></b>
+          <FormattedMessage id="location_content_two" />
+          <FormattedMessage id="location_content_three" />
+          <FormattedMessage id="location_content_email" />
+        </Typography>
+        <Typography gutterBottom variant="subheading" className={classes.body}>
+          <b><FormattedMessage id="location_content_four" /></b>
+          <FormattedMessage id="location_content_five" />
+          <FormattedMessage id="location_content_six" />
+          <a href="https://www.basic-fit.com/en-be/fitness/personal-support/details?trainerid=sofie-de-backer&clubid=155cea3241534e20b82795381f8eae17">
+            <FormattedMessage id="location_content_bf_contact" />
+          </a>
+        </Typography>
       </div>
-        </Grid>
     </Grid>
+  </Grid>
 );
 
 Location.propTypes = {
